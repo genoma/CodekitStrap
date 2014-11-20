2b83cbc27aa092e30f8a869091ddbd8501f57166
Alessandro Vioni <jenoma@gmail.com>
2014-11-20 10:02:50 +0100

    :sob:

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-var gulpStrap;gulpStrap=angular.module("testApp",[]),gulpStrap.controller("Head",function(t){t.header=[{title:"HEADER",snippet:"this is an header"}]}),gulpStrap.controller("test",function(t,e){e.get("https://api.coindesk.com/v1/bpi/currentprice.json").success(function(e){t.btc=e}).error(function(t){return console.log(t)})}),$(function(){return console.log("G")});
+var gulpStrap;
+
+gulpStrap = angular.module("testApp", []);
+
+gulpStrap.controller("Head", function($scope) {
+  $scope.header = [
+    {
+      title: "HEADER",
+      snippet: "this is an header"
+    }
+  ];
+});
+
+gulpStrap.controller("test", function($scope, $http) {
+  $http.get("https://api.coindesk.com/v1/bpi/currentprice.json").success(function(data, status, headers, config) {
+    $scope.btc = data;
+  }).error(function(data, status, headers, config) {
+    return console.log(data);
+  });
+});
+
+$(function() {
+  return console.log("G");
+});
+
+//# sourceMappingURL=../maps/app.js.map
