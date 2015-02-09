7b477942f45727816ea9257f31315ef2f3aa2529
Alessandro Vioni <jenoma@gmail.com>
2015-02-09 18:02:08 +0100

    Updated

--- gulpfile.js
+++ gulpfile.js
@@ -2,37 +2,38 @@ var gulp = require('gulp');
 var del = require('del');
 
 // Clean the dist folder
-gulp.task('clean', function() {
-   return del(['dist/*']);
+gulp.task('clean', function(cb) {
+   return del(['dist'], cb);
 });
 
 // Move the needed files and folders into a dist folder which can be deployed to the webserver
 gulp.task('move', ['clean'], function() {
-  gulp.src(['./bower_components/**/*.*', './css/**/*.*', './js/**/*.*', './*.html', './templates/**/*.*', './images/**/*.*', './.htaccess'], { base: './' })
+  gulp.src(['./bower_components/**/*.*', './css/**/*.*', './js/**/*.*', './*.html', './templates/**/*.*', './images/**/*.*', './.htaccess', './fonts/**/*.*', './functions/**/*.*', './contents/**/*.*'], { base: './' })
   .pipe(gulp.dest('dist'));
 });
 
 
-//
-// FINAL TASKS
-//
+// Prepare the project after a
+// Bootstrap submodule update
 
-// PREPARE THE PROJECT AFTER A
-// BOOTSTRAP SUBMODULE UPDATE
-
-// Copy everything from Bootstrap to
-// root JS folder
-gulp.task('pre-prepare', function() {
+gulp.task('pre-prepare', function(cb) {
   gulp.src(['./bootstrap/js/**/*.*/'], {base: './bootstrap/js'})
   .pipe(gulp.dest('js'));
+  setTimeout(function(){
+    cb(null);
+  }, 500);
+});
+
+gulp.task('font-prepare', function() {
+  gulp.src(['./bootstrap/fonts/**/*.*/'], {base: './bootstrap/fonts'})
+  .pipe(gulp.dest('fonts'));
 });
 
 // Delete the tests folder
-gulp.task('post-prepare', function() {
-  return del(['js/tests']);
+gulp.task('post-prepare', ['pre-prepare'], function() {
+  return del(['./js/tests']);
 });
 
 // Assemble the final task
-gulp.task('prepare', ['pre-prepare', 'post-prepare']);
-
-gulp.task('dist', ['clean', 'move']);
+gulp.task('prepare', ['pre-prepare', 'font-prepare']);
+gulp.task('dist', ['move']);
